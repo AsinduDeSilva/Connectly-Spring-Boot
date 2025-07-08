e99e4f65e1bb17f6c7ece5fc0517281bf29ead84
Sandeep Nilwakka <twinsandeep@gmail.com>
2025-07-08 12:47:02 +0530

    Implemented feat:add-post

--- src/main/java/com/example/demo/controller/PostController.java
+++ src/main/java/com/example/demo/controller/PostController.java
@@ -1,6 +1,9 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.PostDTO;
 import com.example.demo.service.PostService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +15,10 @@ public class PostController {
 
     public PostController(PostService postService) {
         this.postService = postService;
+    }
+
+    @PostMapping
+    public PostDTO createPost(@RequestBody PostDTO postDTO){
+        return postService.createPost(postDTO);
     }
 }
--- src/main/java/com/example/demo/service/impl/PostServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/PostServiceImpl.java
@@ -1,15 +1,28 @@
 package com.example.demo.service.impl;
 
+import com.example.demo.dto.PostDTO;
+import com.example.demo.model.Post;
 import com.example.demo.repository.PostRepository;
 import com.example.demo.service.PostService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PostServiceImpl implements PostService {
 
     private final PostRepository postRepository;
+    private final ModelMapper modelMapper;
 
-    public PostServiceImpl(PostRepository postRepository) {
+    @Autowired
+    public PostServiceImpl(PostRepository postRepository, ModelMapper modelMapper) {
         this.postRepository = postRepository;
+        this.modelMapper = modelMapper;
+    }
+
+    @Override
+    public PostDTO createPost(PostDTO postDTO){
+
+        return modelMapper.map(postRepository.save(modelMapper.map(postDTO, Post.class)), PostDTO.class);
     }
 }
